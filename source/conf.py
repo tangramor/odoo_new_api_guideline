35246f3accde507d46d3874499ae7b93899a21d0
王俊华 <wangjh@a-li.com.cn>
2016-12-07 17:52:10 +0800

    readthedocs doesn't support imgmath

--- source/conf.py
+++ source/conf.py
@@ -30,7 +30,7 @@ import os
 # ones.
 extensions = [
     'sphinx.ext.intersphinx',
-    'sphinx.ext.imgmath',
+    'sphinx.ext.pngmath',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
